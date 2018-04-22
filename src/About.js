f829336acc7668fb28e458c23aaa59960e11045d
CRHarding <casey.r.harding@gmail.com>
2018-04-22 18:28:05 -0400

    Adjusting about me

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ const About = () => {
         className="flow-text center grey-text text-darken-1"
         style={{ fontSize: '15px' }}
       >
-        I am a trained fiction editor, writer, barista, bartender, server, jazz drummer, barista, and full stack specialist.
+        I am a trained fiction editor, writer, bartender, server, jazz drummer, barista, passionate bread baker, and full stack specialist.
       </p>
       <p
         className="flow-text center grey-text text-darken-1"
