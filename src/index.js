46d13ddcf521d52e5ea67c8d475183359049a926
Casey Harding <casey.r.harding@gmail.com>
2022-04-03 14:53:50 -0500

    Deploying...crosses fingers even more aggressively...even more.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
-
-registerServiceWorker();
